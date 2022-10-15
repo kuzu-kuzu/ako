c3f3b82699005b0c93dbfa06041e007760c6cbb6
sqrtox <84203131+sqrtox@users.noreply.github.com>
2022-10-15 21:37:09 +0900

    add: update

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js';
+import { ApplicationCommandOptionType, ChannelType, Client } from 'discord.js';
 import { config as configDotenv } from 'dotenv';
 import { assertEnv } from '~/util/assertEnv';
 
@@ -10,6 +10,33 @@ const { DISCORD_BOT_TOKEN } = assertEnv([
 
 const client = new Client({
   intents: 0
+});
+
+client.on('ready', async readiedClient => {
+  await readiedClient.application.commands.create({
+    description: 'Set the emoji channel.',
+    descriptionLocalizations: {
+      ja: '絵文字チャンネルを設定します。'
+    },
+    name: 'set_emoji_channel',
+    options: [
+      {
+        channelTypes: [
+          ChannelType.GuildText
+        ],
+        description: 'Emoji Channel.',
+        descriptionLocalizations: {
+          ja: '絵文字チャンネル'
+        },
+        name: 'channel',
+        nameLocalizations: {
+          ja: 'チャンネル'
+        },
+        required: false,
+        type: ApplicationCommandOptionType.Channel
+      }
+    ]
+  });
 });
 
 client.login(DISCORD_BOT_TOKEN);
