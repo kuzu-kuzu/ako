19c2dfb9e3e7ecc6d2098e504b7edd1a0da82b40
sqrtox <84203131+sqrtox@users.noreply.github.com>
2022-10-15 21:55:58 +0900

    add: update

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,16 @@
+import { Base } from '@sqrtox/deta';
 import { ApplicationCommandOptionType, ChannelType, Client } from 'discord.js';
 import { config as configDotenv } from 'dotenv';
 import { assertEnv } from '~/util/assertEnv';
 
 configDotenv();
 
-const { DISCORD_BOT_TOKEN } = assertEnv([
-  'DISCORD_BOT_TOKEN'
+const {
+  DISCORD_BOT_TOKEN,
+  DETA_PROJECT_KEY
+} = assertEnv([
+  'DISCORD_BOT_TOKEN',
+  'DETA_PROJECT_KEY'
 ]);
 
 const client = new Client({
@@ -29,14 +34,51 @@ client.on('ready', async readiedClient => {
           ja: '絵文字チャンネル'
         },
         name: 'channel',
-        nameLocalizations: {
-          ja: 'チャンネル'
-        },
         required: false,
         type: ApplicationCommandOptionType.Channel
       }
     ]
   });
+});
+
+type GuildSettings = Readonly<{
+  emojiChannelId: string
+}>;
+
+const settingsBase = new Base<string, GuildSettings>({
+  name: 'settings',
+  projectKey: DETA_PROJECT_KEY
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+
+  await interaction.deferReply();
+
+  if (!interaction.inGuild()) {
+    await interaction.editReply('ギルド内で実行してください');
+
+    return;
+  }
+
+  const channelOption = interaction.options.get('channel');
+  const channel = channelOption?.channel;
+  const resolvedChannel = channel ?? interaction.channel;
+
+  if (!resolvedChannel) {
+    await interaction.editReply('チャンネルが見つかりませんでした');
+
+    return;
+  }
+
+  await settingsBase.put({
+    emojiChannelId: resolvedChannel.id,
+    key: interaction.guildId
+  });
+
+  await interaction.editReply(`絵文字チャンネルを**${resolvedChannel.name}**に設定しました`);
 });
 
 client.login(DISCORD_BOT_TOKEN);
